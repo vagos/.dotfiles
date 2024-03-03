8dbb2b4d1300fa4154fbf661021504d3f04781a8
Evangelos Lamprou <vagos@lamprou.xyz>
2024-03-03 20:03:34 +0100

    lf: add cleaner, lfcd.sh, update lfrc with preview and cleaner settings, add preview script, add with dragon drop mapping

--- lf/.config/lf/lfcd.sh
+++ lf/.config/lf/lfcd.sh
@@ -14,15 +14,41 @@
 #     bindkey -s '^o' 'lfcd\n'  # zsh
 #
 
+cleanup() {
+  exec 3>&-
+  rm "$FIFO_UEBERZUG"
+}
+
+# if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+#   lf "$@"
+# else
+#   [ ! -d "$HOME/.cache/lf" ] && mkdir --parents "$HOME/.cache/lf"
+#   export FIFO_UEBERZUG="$HOME/.cache/lf/ueberzug-$$"
+#   mkfifo "$FIFO_UEBERZUG"
+#   ueberzug layer -s <"$FIFO_UEBERZUG" -p json &
+#   exec 3>"$FIFO_UEBERZUG"
+#   trap cleanup EXIT
+#   lf "$@" 3>&-
+# fi
+
+
 lfcd () {
     tmp="$(mktemp)"
-    lf -last-dir-path="$tmp" "$@"
+
+    [ ! -d "$HOME/.cache/lf" ] && mkdir --parents "$HOME/.cache/lf"
+    export FIFO_UEBERZUG="$HOME/.cache/lf/ueberzug-$$"
+    mkfifo "$FIFO_UEBERZUG"
+    (ueberzug layer -s <"$FIFO_UEBERZUG" -p json &)
+    exec 3>"$FIFO_UEBERZUG"
+    trap cleanup EXIT
+
+    lf -last-dir-path="$tmp" "$@" 3>&-
     if [ -f "$tmp" ]; then
         dir="$(cat "$tmp")"
         rm -f "$tmp"
         if [ -d "$dir" ]; then
             if [ "$dir" != "$(pwd)" ]; then
-                cd "$dir"
+                cd "$dir" || exit 1
             fi
         fi
     fi
