e40896d969067e737be44478ba2dc7ba6db2c44d
Evangelos Lamprou <vagos@lamprou.xyz>
2025-02-26 02:41:29 +0200

    lf: simplify & remove ueberzeug
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- lf/.config/lf/lfcd.sh
+++ lf/.config/lf/lfcd.sh
@@ -8,48 +8,13 @@
 #         source "$LFCD"
 #     fi
 #
-# You may also like to assign a key to this command:
+# You may also like to assign a key (Ctrl-O) to this command:
 #
 #     bind '"\C-o":"lfcd\C-m"'  # bash
 #     bindkey -s '^o' 'lfcd\n'  # zsh
 #
 
-cleanup() {
-  exec 3>&-
-  rm "$FIFO_UEBERZUG"
-}
-
-# if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-#   lf "$@"
-# else
-#   [ ! -d "$HOME/.cache/lf" ] && mkdir --parents "$HOME/.cache/lf"
-#   export FIFO_UEBERZUG="$HOME/.cache/lf/ueberzug-$$"
-#   mkfifo "$FIFO_UEBERZUG"
-#   ueberzug layer -s <"$FIFO_UEBERZUG" -p json &
-#   exec 3>"$FIFO_UEBERZUG"
-#   trap cleanup EXIT
-#   lf "$@" 3>&-
-# fi
-
-
 lfcd () {
-    tmp="$(mktemp)"
-
-    [ ! -d "$HOME/.cache/lf" ] && mkdir --parents "$HOME/.cache/lf"
-    export FIFO_UEBERZUG="$HOME/.cache/lf/ueberzug-$$"
-    mkfifo "$FIFO_UEBERZUG"
-    (ueberzug layer -s <"$FIFO_UEBERZUG" -p json &)
-    exec 3>"$FIFO_UEBERZUG"
-    trap cleanup EXIT
-
-    lf -last-dir-path="$tmp" "$@" 3>&-
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        rm -f "$tmp"
-        if [ -d "$dir" ]; then
-            if [ "$dir" != "$(pwd)" ]; then
-                cd "$dir" || exit 1
-            fi
-        fi
-    fi
+    # `command` is needed in case `lfcd` is aliased to `lf`
+    cd "$(command lf -print-last-dir "$@")" || exit 1
 }
